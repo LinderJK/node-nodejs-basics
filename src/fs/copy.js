63e07053304b41598df5bfde9872374de1f08506
LinderJK <slepchenkovia@gmail.com>
2024-10-02 00:21:38 +0300

    feat: add solution for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+      await fs.access('./src/fs/files')
+      await fs.access('./src/fs/files_copy')
+
+      throw new Error('FS operation failed');
+
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        if (err.path === './src/fs/files') {
+          throw new Error('FS operation failed');
+        }
+        if (err.path === './src/fs/files_copy') {
+          await fs.mkdir('./src/fs/files_copy');
+        }
+      } else {
+        console.error(err);
+        return;
+      }
+    }
+
+    try {
+      await fs.cp('./src/fs/files', './src/fs/files_copy', {recursive: true});
+    } catch (err) {
+      console.error(err);
+    }
 };
 
 await copy();
