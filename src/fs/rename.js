6f9da4c760872249165ad8628246cd519a58e9f0
LinderJK <slepchenkovia@gmail.com>
2024-10-03 19:13:13 +0300

    feat: add solution for rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+    try {
+      await fs.access('./src/fs/files/wrongFilename.txt')
+      await fs.access('./src/fs/files/properFilename.md')
+
+      throw new Error('FS operation failed');
+
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        if (err.path === './src/fs/files/wrongFilename.txt') {
+          throw new Error('FS operation failed');
+        }
+        if (err.path === './src/fs/files/properFilename.md') {
+          try {
+            await fs.rename('./src/fs/files/wrongFilename.txt', './src/fs/files/properFilename.md');
+            console.log('File renamed successfully!');
+          } catch (renameErr) {
+            console.error('Error during renaming:', renameErr);
+          }
+        }
+      } else {
+        console.error(err);
+      }
+    }
 };
 
 await rename();
