102363f15d18c5ab09c00dfb436a9d7628ecb77f
LinderJK <slepchenkovia@gmail.com>
2024-10-03 20:20:26 +0300

    feat: change import json, change export functions

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -8,12 +8,12 @@ import './files/c.js'
 
 const random = Math.random();
 
-let unknownObject;
+export let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', {assert: {type: 'json'}});
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', {assert: {type: 'json'}});
 }
 
 console.log(`Release ${release()}`);
@@ -23,7 +23,7 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const myServer = createServerHttp((_, res) => {
+export const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
@@ -35,9 +35,4 @@ myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log('To terminate it, use Ctrl+C combination');
 });
-
-module.exports = {
-    unknownObject,
-    myServer,
-};
 
