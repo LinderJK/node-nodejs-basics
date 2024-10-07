e2cfe5149ce380d879c88f9e719f997d75e32593
LinderJK <slepchenkovia@gmail.com>
2024-10-07 22:23:50 +0300

    feat: add imports

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,10 @@
+import { cpus } from 'os';
+import {Worker} from 'worker_threads';
+import path from "path";
+
+const __dirname = import.meta.dirname;
+const filePath = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
     // Write your code here
 };
