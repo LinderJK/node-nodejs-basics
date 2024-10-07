b78138ce280de19536162f2fbb0d35e95d8569ca
LinderJK <slepchenkovia@gmail.com>
2024-10-07 13:32:31 +0000

    feat: add solution for stream write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = import.meta.dirname;
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    try{
+        const stream = createWriteStream(filePath , {encoding: 'utf8'});
+        process.stdin.pipe(stream);
+        stream.on("open", () => {
+          console.log("Use CTRL + D to stop writing to file");
+        });
+        stream.on("error", (err) => {
+            console.error("Error writing file", err);
+        })
+        stream.on("finish", () => {
+            console.log("\n File written successfully");
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await write();
