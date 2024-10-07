3ad3699f776af747be4113e2e24986d63c9a2880
LinderJK <slepchenkovia@gmail.com>
2024-10-07 13:32:50 +0000

    feat: add solution for stream tranform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from 'stream';
+const transform = async () => {;
+    try {
+        const transformStream = new Transform({
+            transform(chunk, _, callback) {
+                const reversed = chunk.toString().split('').reverse().join('') + '\n';
+                callback(null, reversed);
+            }
+        });
+        process.stdin.pipe(transformStream).pipe(process.stdout);
+        transformStream.on('error', (err) => {
+            console.error(err);
+        })
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await transform();
