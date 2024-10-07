5f7b44e6e2c4270935c0c92a3a785762877e3bdc
LinderJK <slepchenkovia@gmail.com>
2024-10-07 22:26:28 +0300

    feat: add create workers based on cpu count

--- src/wt/main.js
+++ src/wt/main.js
@@ -9,6 +9,37 @@ const performCalculations = async () => {
     let n = 10;
     const cpu = cpus().length;
 
+    const workers = Array.from({length: cpu}, (_,index) => {
+    return new Promise((resolve,reject)=> {
+      const worker = new Worker(filePath, {
+        workerData: n + index
+      })
+
+      worker.on('message', (result) => {
+        resolve({
+          status: 'resolved',
+          data: result
+        });
+      })
+      worker.on('error', () => {
+        resolve({
+          status: 'error',
+          data: null
+        });
+      })
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          resolve({
+            status: 'error',
+            data: null
+          })
+        }
+      })
+    })
+  })
+
+  const results = await Promise.allSettled(workers);
+  console.log(results);
 
 };
 
