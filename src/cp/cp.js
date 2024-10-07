94024d712b433bc0ccb64637bbdd08f58e8fdbe7
LinderJK <slepchenkovia@gmail.com>
2024-10-07 23:14:51 +0300

    feat: add imports, create child process with fork

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import {fork} from 'child_process';
+import path from "path";
+
+const __dirname = import.meta.dirname;
+const filePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  try {
+    const child = fork(filePath, args);
+    child.on('message', (message) => {
+      console.log(message);
+    });
+    child.on('error', (err) => {
+      console.error(err);
+    });
+    child.on('exit', (code) => {
+      if (code !== 0) {
+        console.error(`Child process exited with code ${code}`);
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(  [2, 5, 6, 8]);
